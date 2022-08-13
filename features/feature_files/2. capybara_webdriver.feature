a425f626b41c461a5cb2a63a0c08a992b47e5a0c
John Williams <john.williams@dvla.gov.uk>
2022-08-13 18:49:51 +0100

    using ruby mine commit option

--- features/feature_files/2. capybara_webdriver.feature
+++ features/feature_files/2. capybara_webdriver.feature
@@ -1,8 +1,13 @@
 Feature: Capybara drivers
 
+  Scenario: I visit google using selenium default driver
+    Given I visit google using selenium default driver
 
   Scenario: I visit google using simple capybara
     Given I visit google using simple capybara
 
+  Scenario: I visit google and pass options to the browser
+    Given I visit google and pass options to the browser
+    
   Scenario: I visit google using remote selenium
     Given I visit google using remote selenium
--- features/step_definitions/2_capybara_steps.rb
+++ features/step_definitions/2_capybara_steps.rb
@@ -1,23 +1,53 @@
+Given(/^I visit google using selenium default driver$/) do
+  set_up_selenium_default_driver
+  visit 'https://www.gov.uk/'
+  sleep 5
+end
+
+def set_up_selenium_default_driver
+  # Capybara pre-registers a number of named drivers that use Selenium - they are:
+  # :selenium => Selenium driving Firefox
+  # :selenium_headless => Selenium driving Firefox in a headless configuration
+  # :selenium_chrome => Selenium driving Chrome
+  # :selenium_chrome_headless => Selenium driving Chrome in a headless configuration
+  Capybara.default_driver = :selenium
+end
+
 Given(/^I visit google using simple capybara$/) do
   set_up_capybara_driver
-  visit 'https://www.google.co.uk/'
+  visit 'https://www.gov.uk/'
   sleep 5
 end
 
 def set_up_capybara_driver
   # Selenium communicates with the chrome web browser
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  Capybara.register_driver :john do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
-  Capybara.default_driver = :chrome
+  Capybara.default_driver = :john
   Capybara.use_default_driver
 end
 
+Given(/^I visit google and pass options to the browser$/) do
+  set_up_capybara_driver_with_options
+  visit 'https://www.gov.uk/'
+  sleep 5
+end
 
+def set_up_capybara_driver_with_options
+  Capybara.register_driver :john do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument '--start-fullscreen'
+    options.add_argument '--window-size=1920,1080'
+    options.add_argument '--ignore-certificate-errors'
+    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
+  end
+  Capybara.default_driver = :john
+end
 
 Given(/^I visit google using remote selenium$/) do
   set_up_capybara_remote_driver
-  visit 'https://www.google.co.uk/'
+  visit 'https://www.gov.uk/'
   sleep 50
 end
 
@@ -38,3 +68,4 @@ def set_up_capybara_remote_driver
   Capybara.default_driver = :remote_selenium
   Capybara.use_default_driver
 end
+
